e899f0c3e9b482ac506053d3498960894b178dcb
Pratik Mahadev Dimble <31777260+pratikdimble@users.noreply.github.com>
2017-12-25 00:22:33 -0800

    Create GoogleServlet.java

---
+++ com/app/GoogleServlet.java
@@ -0,0 +1,25 @@
+package com.app;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class GoogleServlet
+ */
+public class GoogleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	     String name=request.getParameter("name");  
+	        response.sendRedirect("https://www.google.co.in/#q="+name);  
+	}
+
+
+
+}
